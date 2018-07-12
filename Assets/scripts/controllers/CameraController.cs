f0dd1170f611880ae296159806f99bff7d04211f
Matt Payne <matthew.payne1988@gmail.com>
2018-07-12 16:36:45 +1200

    lots of changes, i need to improve this

--- Assets/scripts/controllers/CameraController.cs
+++ Assets/scripts/controllers/CameraController.cs
@@ -4,42 +4,45 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    [Header("Camera Components")]
+    public GameObject camStartPoint;
+    public GameObject camEndPoint;
+
+    [Header("Camera Control Variables")]
+    public float camDollyUpSpeed = 0f;
+    public float camDollyDownSpeed = 0f;
     public float camZoomInSpeed = 150f;
     public float camZoomOutSpeed = 10f;
-
-    private Camera mainCam;
-    private GameObject player;
-    private Vector3 camOffset;
-
-    private void Awake()
-    {
-        player = GameObject.FindWithTag("Player");
-        mainCam = Camera.main;
-        camOffset = mainCam.transform.position - player.transform.position;
-    }
 
     private void LateUpdate()
     {
         CamMove();
-        CamFOVControl();
+        //CamFOVControl();
     }
 
+    //Not working look at fixing with a simple rotational offset on a gameobject parented to the player
     void CamMove()
     {
-        //Need to make this lerp between these positions using Vector3.lerp
+        /*
+        float x = transform.rotation.x;
+
         if (PlayerMovement.isFalling == false)
         {
-            mainCam.transform.position = player.transform.position + camOffset;
+            transform.position = Vector3.Lerp(transform.position, camStartPoint.transform.position, Time.time / camDollyDownSpeed);
+            //x = Mathf.Lerp(camEndPoint.transform.rotation.x, camStartPoint.transform.rotation.x, Time.time / camDollyDownSpeed);
         }
-        else
+        else if (PlayerMovement.isFalling == true)
         {
-            mainCam.transform.position = player.transform.position + camOffset + new Vector3(0f, 30f, 0f);
+            transform.position = Vector3.Lerp(transform.position, camEndPoint.transform.position, Time.time / camDollyUpSpeed);
+            //x = Mathf.Lerp(camStartPoint.transform.rotation.x, camEndPoint.transform.rotation.x, Time.time / camDollyDownSpeed);
         }
-        
+        */
     }
 
     void CamFOVControl()
     {
+        Camera mainCam = Camera.main;;
+        
         if (PlayerMovement.isFalling == false)
         {
             mainCam.fieldOfView = Mathf.Clamp(mainCam.fieldOfView - (1f * Time.deltaTime) * camZoomInSpeed, 60f, 90f);
---
+++ Assets/scripts/debug/DebugPosition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugPosition : MonoBehaviour {
+
+    public Color color = Color.red;
+    public float radius = 1f;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
