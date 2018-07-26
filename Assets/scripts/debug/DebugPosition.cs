873b7a47540a4e6a031404d5c8495961a1b7ef49
Matt Payne <matthew.payne1988@gmail.com>
2018-07-27 08:17:33 +1200

    DebugPosition.cs
    manualy reverted the code

--- Assets/scripts/debug/DebugPosition.cs
+++ Assets/scripts/debug/DebugPosition.cs
@@ -4,45 +4,16 @@ using UnityEngine;
 
 public class DebugPosition : MonoBehaviour {
 
-    public GameObject ancorPoint;
-
-    public float distanceMax = 0f;
-    public float distanceMin = 0f;
 
     public Color colorCorrect = Color.cyan;
     public Color colorError = Color.red;
     public float radius = 1f;
 
-    private bool isFar = false;
 
     private void OnDrawGizmos()
     {
-        GetDistance();
-
-        if (isFar == false)
-        {
-            Gizmos.color = colorCorrect;
-        }
-        else
-        {
-            Gizmos.color = colorError;
-        }
-
+        Gizmos.color = colorCorrect;
         Gizmos.DrawSphere(transform.position, radius);
     }
 
-
-    void GetDistance()
-    {
-        float d = Vector3.Distance(ancorPoint.transform.position, transform.position);
-
-        if (d < distanceMin || d > distanceMax)
-        {
-            isFar = true;
-        }
-        else
-        {
-            isFar = false;
-        }
-    }   
 }
