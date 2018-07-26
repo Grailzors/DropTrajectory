d152147b9c66673dcdb9085550ee917e7abb8326
Matt Payne <matthew.payne1988@gmail.com>
2018-07-26 13:43:00 +1200

    Distance Testing
    added new scene and DebugPosition.cs update

--- Assets/scripts/debug/DebugPosition.cs
+++ Assets/scripts/debug/DebugPosition.cs
@@ -4,12 +4,45 @@ using UnityEngine;
 
 public class DebugPosition : MonoBehaviour {
 
-    public Color color = Color.red;
+    public GameObject ancorPoint;
+
+    public float distanceMax = 0f;
+    public float distanceMin = 0f;
+
+    public Color colorCorrect = Color.cyan;
+    public Color colorError = Color.red;
     public float radius = 1f;
+
+    private bool isFar = false;
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = color;
+        GetDistance();
+
+        if (isFar == false)
+        {
+            Gizmos.color = colorCorrect;
+        }
+        else
+        {
+            Gizmos.color = colorError;
+        }
+
         Gizmos.DrawSphere(transform.position, radius);
     }
+
+
+    void GetDistance()
+    {
+        float d = Vector3.Distance(ancorPoint.transform.position, transform.position);
+
+        if (d < distanceMin || d > distanceMax)
+        {
+            isFar = true;
+        }
+        else
+        {
+            isFar = false;
+        }
+    }   
 }
