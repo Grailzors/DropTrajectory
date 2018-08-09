6a2ca8e4e5e3819c63dcedb1e7b6105b144479f0
Matt Payne <matthew.payne1988@gmail.com>
2018-08-10 08:07:16 +1200

    PlayerManager.cs update

--- Assets/scripts/managers/PlayerManager.cs
+++ Assets/scripts/managers/PlayerManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class PlayerManager : MonoBehaviour {
 
     [Header("Player Components")]
-    public GameObject rayPoint;
 
     [Header("Player Variables")]
     public int playerLivesLimit;
@@ -17,14 +16,21 @@ public class PlayerManager : MonoBehaviour {
     public ParticleSystem[] fallParticles;
     public ParticleSystem[] tireSmoke;
 
+    [Header("Score Multiplier")]
+    public float bounceAmount = 1f;
+    public float coolDownTimer = 2f;
+    public float coolDownSpeed = 0.5f;
+
     public static GameObject player;
-    public static int playerScore;
+    public static float playerScore;
+    public static float multiplierScore;
+    public static float multiplierTimer;
     public static float screenFadeValue;
     public static float resetCounter;
     public static float fallTimer;
 
     private Rigidbody playerRB;
-    //private GameObject rayTarget;
+    private int multiplierCollisions;
 
     private void Start()
     {
@@ -49,6 +55,7 @@ public class PlayerManager : MonoBehaviour {
 
         //Maybe find a better way for this to be done? coroutine?
         PlayTireSmoke();
+        MultiplyUpdate();
 
         fallTimer = fallTimeOut;
     }
@@ -62,6 +69,7 @@ public class PlayerManager : MonoBehaviour {
     private void OnCollisionEnter(Collision col)
     {
         ResetSpawnPos(col);
+        MultiplierIncrease(col);
     }
 
     void ResetSpawnPos(Collision col)
@@ -93,23 +101,86 @@ public class PlayerManager : MonoBehaviour {
         }
     }
 
+    //Controlls passing the collect points of the character to the GM script
     void BankScore(Collider other)
     {
         if (other.tag == "Bank")
         {
-            GM.bankScore += playerScore;
+            GM.bankScore += playerScore * multiplierScore;
             playerScore = 0;
             GM.isBanked = true;
+            multiplierTimer = 0f;
+            multiplierScore = 0f;
         }
     }
 
+    //Controls the amount of points the character has collected so far before banking
     void PlayerScore(Collider other)
     {
         if (other.tag == "PickUp")
         {
+            multiplierTimer = coolDownTimer;
+            multiplierCollisions += 1;
             playerScore += 1;
             GM.isBanked = true;
         }
+    }
+
+    void MultiplierIncrease(Collision col)
+    {
+        if (col.gameObject.tag == "Platform" || col.gameObject.tag == "PickUp")
+        {
+            if (multiplierTimer > 0)
+            {
+                //increase multiplier timer per bounce/hit of a platform
+                multiplierTimer += bounceAmount;
+                multiplierTimer = Mathf.Clamp(multiplierTimer, 0, coolDownTimer);
+            }
+        }
+    }
+
+    void MultiplyUpdate()
+    {
+        //Limit the amount of collisions detected so that the multiplier doesn't go above 
+        //a value of 30
+        multiplierCollisions = Mathf.Clamp(multiplierCollisions, 0, 5);
+
+        if (multiplierTimer > 0f)
+        {
+            //cooldown for the multiplier effect
+            multiplierTimer -= coolDownSpeed * Time.deltaTime;
+
+            //Set multiplier score
+            switch (multiplierCollisions)
+            {
+                case 5:
+                    multiplierScore = 30f;
+                    break;
+                case 4:
+                    multiplierScore = 15f;
+                    break;
+                case 3:
+                    multiplierScore = 5f;
+                    break;
+                case 2:
+                    multiplierScore = 2f;
+                    break;
+                case 1:
+                    multiplierScore = 1.5f;
+                    break;
+                default:
+                    multiplierScore = 1f;
+                    break;
+            }
+        }
+        else if (multiplierTimer <= 0f)
+        {
+            multiplierTimer = 0f;
+            multiplierScore = 0f;
+        }
+
+        print(multiplierScore);
+
     }
 
     void MoveToStartPos()
@@ -132,6 +203,8 @@ public class PlayerManager : MonoBehaviour {
             playerRB.velocity = new Vector3();
             GM.playerLives -= 1;
             playerScore = 0;
+            multiplierTimer = 0f;
+            multiplierScore = 0f;
         }
 
         //Set and reset counter depending if player is falling
